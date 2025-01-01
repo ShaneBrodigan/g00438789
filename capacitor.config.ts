a398d80e3882cc54f52bf715af1abcc1c382dab2
Shane Brodigan <shanebrodigan1@gmail.com>
2025-01-01 22:49:48 +0000

    First commit, project setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Gg00438789',
+  webDir: 'www'
+};
+
+export default config;
