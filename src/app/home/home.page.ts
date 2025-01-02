45195bda3c28562615268548da194275d3e38571
Shane Brodigan <shanebrodigan1@gmail.com>
2025-01-02 23:10:12 +0000

    Added button and search bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton} from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton],
 })
 export class HomePage {
+
+  //instance var to store user input
+  searchedCountry: string = "";
+
   constructor() {}
+
+  openCountriesPage() {
+    
+  }
 }
