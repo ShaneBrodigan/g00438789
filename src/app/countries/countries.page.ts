9c9f196069d5cb5ae82fd9401ebbc4b3e8b9f793
Shane Brodigan <shanebrodigan1@gmail.com>
2025-01-03 16:37:45 +0000

    Added countries media card, button and started on http service

--- src/app/countries/countries.page.ts
+++ src/app/countries/countries.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { MyDataService } from '../services/my-data.service';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.page.html',
   styleUrls: ['./countries.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCardSubtitle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class CountriesPage implements OnInit {
 
-  constructor() { }
+  searchedCountry: string= "";
+
+  constructor(private mds: MyDataService) { }
 
   ngOnInit() {
+    this.getSearchedCountry();
+  }
+
+  async getSearchedCountry() {
+    this.searchedCountry = await this.mds.get('countryName');
   }
 
 }
