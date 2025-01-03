c2752a9723db1518ea3a16a405f83b9c3e9a0707
Shane Brodigan <shanebrodigan1@gmail.com>
2025-01-03 01:47:02 +0000

    Installed Storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton} from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
+import { AppDataService } from '../services/app-data.service';
+import { CommonModule } from '@angular/common';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton],
+  imports: [CommonModule, FormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonicStorageModule],
 })
 export class HomePage {
 
-  //instance var to store user input
+  ads: AppDataService;
   searchedCountry: string = "";
 
-  constructor() {}
 
-  openCountriesPage() {
-    
+  constructor(private ads: AppDataService) {
+   this.ads = ads;
+  }
+
+  ngOnInit() {}
+
+  async openCountriesPage() {
+    await this.ads.set("kw", this.searchedCountry);
   }
 }
