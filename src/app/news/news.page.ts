743aed84f5245f5836253fefc4554f95cefb25ab
Shane Brodigan <shanebrodigan1@gmail.com>
2025-01-03 23:27:06 +0000

    News API connected and working on page

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -5,6 +5,7 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCard, 
 import { Router } from '@angular/router';
 import { MyDataService } from '../services/my-data.service';
 import { MyHttpService } from '../services/my-http.service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-news',
@@ -15,21 +16,34 @@ import { MyHttpService } from '../services/my-http.service';
 })
 export class NewsPage implements OnInit {
 
-  Countrycca2: string = "";
-  CountryName: string = "";
+  countrycca2: string = "";
+  countryName: string = "";
+  apiKey = "pub_64298dc8e5a64e22345a37529de6097a947d0";
+  newsRecievedFromApi: any;
+  options: HttpOptions = {
+    url: "https://newsdata.io/api/1/latest?apikey=" + this.apiKey + "&country="
+  }
 
   constructor(private mds: MyDataService, private mhs: MyHttpService) { }
 
   ngOnInit() {
     this.getCountryCca2();
-    this.getCountryName();
   }
 
   async getCountryCca2() {
-    this.Countrycca2 = await this.mds.get("cca2");
+    this.countrycca2 = await this.mds.get("cca2OfCountry");
+    this.getCountryName();
   }
 
   async getCountryName() {
-    this.CountryName = await this.mds.get("nameOfCountry");
+    this.countryName = await this.mds.get("nameOfCountry");
+    this.getCountryNews();
+  }
+
+  async getCountryNews() {
+    this.options.url = this.options.url.concat(this.countrycca2);
+    let result = await this.mhs.get(this.options); 
+    this.newsRecievedFromApi = result.data.results;
+    console.log(this.newsRecievedFromApi);
   }
 }
