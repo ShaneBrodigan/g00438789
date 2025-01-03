67d41c8abcc7a90e272e93215b9558109afac5c2
Shane Brodigan <shanebrodigan1@gmail.com>
2025-01-03 10:11:15 +0000

    Home Screen with new button and input

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MyDataService } from '../services/my-data.service';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, FormsModule, CommonModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, FormsModule, CommonModule],
 })
 export class HomePage {
   mds: MyDataService;
@@ -27,7 +27,7 @@ export class HomePage {
   }
 
   async setCountry() {
-    await this.mds.set("name", this.searchedCountry);
+    await this.mds.set("countryName", this.searchedCountry);
   }
 
   /*
